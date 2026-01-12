34c2990792202e989468e4d9268879e9be2a10c6
lumoshive-academy <lumoshive.academy@gmail.com>
2026-01-12 16:15:21 +0700

    fix response

--- dto/response.go
+++
@@ -1,9 +0,0 @@
-package dto
-
-// Struct api response
-type APIResponse struct {
-	Status  string `json:"status"`
-	Code    int    `json:"code"`
-	Message string `json:"message"`
-	Error   string `json:"error,omitempty"`
-}
